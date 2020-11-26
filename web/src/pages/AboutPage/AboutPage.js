25e862ab82577597e8a7b505b34b80055bdd75d8
Kim, Janghwan <janghwan@gmail.com>
2020-11-26 18:34:44 +0900

    ADD following upto Layouts in tutorial
    https://redwoodjs.com/tutorial/layouts

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,15 @@
+import {Link, routes} from '@redwoodjs/router'
+
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout';
+
+const AboutPage = () => {
+  return (
+    <BlogLayout>
+      <p>
+        This site was constructed to deomonstrate my mastery of Redwood: Look on my works, ye mighty, and despair!
+      </p>
+    </BlogLayout>
+  )
+}
+
+export default AboutPage
