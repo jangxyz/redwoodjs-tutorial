fa5cb95d3a0d7590b794d82f625523ca90650768
Kim, Janghwan <janghwan@gmail.com>
2020-11-30 02:56:30 +0900

    ADD following upto 'Saving Data' in tutorial
    https://redwoodjs.com/tutorial/saving-data
    
    also save separate prettier package so webstorm can recognize

--- web/src/pages/ContactPage/ContactPage.js
+++ web/src/pages/ContactPage/ContactPage.js
@@ -1,34 +1,100 @@
-import {Form, Label, TextField, TextAreaField, Submit, FieldError} from '@redwoodjs/forms'
+import { Flash, useFlash, useMutation } from '@redwoodjs/web'
+import {
+  Form,
+  Label,
+  TextField,
+  TextAreaField,
+  Submit,
+  FieldError,
+  FormError,
+} from '@redwoodjs/forms'
+import { useForm } from 'react-hook-form'
 
-import BlogLayout from "src/layouts/BlogLayout/BlogLayout";
+import BlogLayout from 'src/layouts/BlogLayout/BlogLayout'
+
+const CREATE_CONTACT = gql`
+  mutation CreateContactMutation($input: CreateContactInput!) {
+    createContact(input: $input) {
+      id
+    }
+  }
+`
 
 const ContactPage = () => {
-  const onSubmit = (data) => {
+  const { addMessage } = useFlash()
+  const formMethods = useForm({ mode: 'onBlur' });
+  const [create, { loading, error }] = useMutation(CREATE_CONTACT, {
+    onCompleted() {
+      addMessage('Thank you for your submission!', {
+        style: { backgroundColor: 'green', color: 'white', padding: '1rem' },
+      })
+      formMethods.reset();
+    },
+  })
+
+  const onSubmit = async (data) => {
     console.log(data)
+    try {
+      await create({ variables: { input: data } })
+    } catch (err) {
+      console.error(err)
+    }
   }
 
   return (
     <BlogLayout>
-      <Form onSubmit={onSubmit} validation={{mode: 'onBlur'}}>
-        <Label name="name" errorClassName="error">Name</Label>
-        <TextField name="name" validation={{required: true}} errorClassName="error"/>
-        <FieldError name="name" className="error"/>
+      formMethods.reset();
+      <Flash timeout={2000} />
+      <Form
+        onSubmit={onSubmit}
+        validation={{ mode: 'onBlur' }}
+        error={error}
+        formMethods={formMethods}
+      >
+        <FormError
+          error={error}
+          wrapperStyle={{
+            color: 'red',
+            backgroundColor: 'lavenderblush',
+          }}
+        />
+        <Label name="name" errorClassName="error">
+          Name
+        </Label>
+        <TextField
+          name="name"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="name" className="error" />
 
-        <Label name="email" errorClassName="error">Email</Label>
-        <TextField name="email" validation={{
-          required: true,
-          pattern: {
-            value: /[^@]+@[^.]+[.].+/,
-            message: 'Please enter a valid email address',
-          },
-        }} errorClassName="error"/>
-        <FieldError name="email" className="error"/>
+        <Label name="email" errorClassName="error">
+          Email
+        </Label>
+        <TextField
+          name="email"
+          validation={{
+            required: true,
+            pattern: {
+              value: /[^@]+@[^.]+[.].+/,
+              message: 'Please enter a valid email address',
+            },
+          }}
+          errorClassName="error"
+        />
+        <FieldError name="email" className="error" />
 
-        <Label name="message" errorClassName="error">Message</Label>
-        <TextAreaField name="message" validation={{required: true}} errorClassName="error"/>
-        <FieldError name="message" className="error"/>
+        <Label name="message" errorClassName="error">
+          Message
+        </Label>
+        <TextAreaField
+          name="message"
+          validation={{ required: true }}
+          errorClassName="error"
+        />
+        <FieldError name="message" className="error" />
 
-        <Submit>Save</Submit>
+        <Submit disabled={loading}>Save</Submit>
       </Form>
     </BlogLayout>
   )
